bc1f8da5c6a2945cb9a487d7510457a1ba1b6d5f
Emmanuel Obeng <emmallob14@gmail.com>
2024-09-12 17:37:44 +0000

    add path

--- application/views/default/main.php
+++ application/views/default/main.php
@@ -14,7 +14,7 @@ $set_current_url = !empty($set_current_url) ? $set_current_url : str_ireplace("/
 
 // get the url path
 $urlParse = parse_url($set_current_url);
-if(empty($urlParse)) {
+if(empty($urlParse['path'])) {
     $set_current_url .= "dashboard";
 }
 ?>
