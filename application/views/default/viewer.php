2f78fae7deddcd085aaf9ed95d9ce0baeab89ba6
Emmanuel Obeng <emmallob14@gmail.com>
2024-11-06 21:48:40 +0000

    add additional properties to the pdf document

--- application/views/default/viewer.php
+++ application/views/default/viewer.php
@@ -4,4 +4,12 @@ if(empty($filePath)) {
   die("File path is required");
 }
 ?>
-<iframe src="<?= $filePath ?>" width="100%" height="100%" frameborder="0"></iframe>
+<iframe 
+  src="<?= $filePath ?>" 
+  width="100%" 
+  height="100%" 
+  loading="lazy" 
+  allowfullscreen 
+  frameborder="0"
+  style="border: none; box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.2);"
+></iframe>
