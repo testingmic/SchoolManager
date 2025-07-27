560d574660afd6b0c01238565e226a4c02157d53
Emmanuel Obeng <emmallob14@gmail.com>
2025-07-27 21:53:27 +0000

    Add google analytics to the homepage

--- website/index.php
+++ website/index.php
@@ -25,6 +25,14 @@ $registerUrl = "https://app.myschoolgh.com/register";
     <link rel='shortcut icon' type='image/x-icon' href='<?= $appUrl ?>/assets/img/favicon.ico' />
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-6YKXX6Z3QZ"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-6YKXX6Z3QZ');
+        </script></head>
     <style>
         * {
             margin: 0;
