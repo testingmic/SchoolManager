709dc58f7dd41858dd64eba946a0ad086b536828
Emmanuel Obeng <emmallob14@gmail.com>
2025-08-05 19:30:32 +0000

    Remove the messags

--- application/models/crons.php
+++ application/models/crons.php
@@ -205,10 +205,8 @@ class Crons {
 
 		print "Setting the configuration params.\n";
 
-		print_r($config);
-
 		// additional settings
-		$mailer->SMTPDebug = SMTP::DEBUG_CLIENT;
+		$mailer->SMTPDebug = SMTP::DEBUG_OFF;
 		$mailer->isSMTP();
 		$mailer->Host = $config->SmtpHost;
 		$mailer->SMTPAuth = true;
