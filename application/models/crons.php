9bd34fdf40f34320a1ad750f084c61d01172b7e9
Emmanuel Obeng <emmallob14@gmail.com>
2025-08-05 19:25:10 +0000

    Print errors in smtp

--- application/models/crons.php
+++ application/models/crons.php
@@ -206,7 +206,7 @@ class Crons {
 		print "Setting the configuration params.\n";
 
 		// additional settings
-		$mailer->SMTPDebug = SMTP::DEBUG_OFF;
+		$mailer->SMTPDebug = SMTP::DEBUG_CLIENT;
 		$mailer->isSMTP();
 		$mailer->Host = $config->SmtpHost;
 		$mailer->SMTPAuth = true;
