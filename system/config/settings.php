f47aa9fbda18945b2af194b74773e96f7e9ccc7d
Emmanuel Obeng <emmallob14@gmail.com>
2025-08-17 22:49:53 +0000

    Update border color for list items to improve visual consistency

--- system/config/settings.php
+++ system/config/settings.php
@@ -16,7 +16,7 @@ define('APPPATH', $application_folder.DIRECTORY_SEPARATOR);
 define('VIEWPATH', $application_folder.DIRECTORY_SEPARATOR);
 
 function version() {
-    return "1.0.62";
+    return "1.0.63";
 }
 
 function loggedIn() {
