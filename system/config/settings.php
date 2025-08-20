dc17781e8a1ea3977cce41a71b851e8393341367
Emmanuel Obeng <emmallob14@gmail.com>
2025-08-20 09:55:24 +0000

    Increament the version number

--- system/config/settings.php
+++ system/config/settings.php
@@ -16,7 +16,7 @@ define('APPPATH', $application_folder.DIRECTORY_SEPARATOR);
 define('VIEWPATH', $application_folder.DIRECTORY_SEPARATOR);
 
 function version() {
-    return "1.0.63";
+    return "1.0.70";
 }
 
 function loggedIn() {
