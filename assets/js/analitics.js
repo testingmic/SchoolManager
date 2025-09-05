c8db2e6e447313346deab08317f35ffdeb8e4d03
Emmanuel Obeng <emmallob14@gmail.com>
2025-09-05 19:07:48 +0000

    Refactor class listing logic in analytics.js for improved readability and maintainability
    - Removed unnecessary conditional check for class count display.
    - Streamlined the class count list generation by consolidating HTML structure.
    - Ensured consistent rendering of class information regardless of count.

--- assets/js/analitics.js
+++ assets/js/analitics.js
@@ -358,7 +358,6 @@ var summaryReporting = (t_summary, date_range) => {
         $(`div[id="class_count_list"]`).html(no_class_found);
     }
 
-
     if (typeof summary.students_class_record_count !== 'undefined') {
         let classes = summary.students_class_record_count,
             class_count_list = "",
@@ -368,17 +367,14 @@ var summaryReporting = (t_summary, date_range) => {
 
         $.each(classes.count, function(i, e) {
             key++;
-            if(e.value > 0) {
-                class_count_list += `
-                    <div class="m-b-20">
-                        <div class="text-small float-right font-17">${e.value}</div>
-                        <div><span onclick="return load('class/${e.class_id}');" class="user_name">${e.name}</span></div>
-                        <div class="progress" data-height="5" style="height: 5px;">
-                        <div class="progress-bar ${bg_colors[key]}" role="progressbar" data-width="${e.percentage}%" aria-valuenow="${e.percentage}" aria-valuemin="0" aria-valuemax="100" style="width: ${e.percentage}%;"></div>
-                        </div>
-                    </div>
-                `;
-            }
+            class_count_list += `
+            <div class="m-b-20">
+                <div class="text-small float-right font-17">${e.value}</div>
+                <div><span onclick="return load('class/${e.class_id}');" class="user_name">${e.name}</span></div>
+                <div class="progress" data-height="5" style="height: 5px;">
+                <div class="progress-bar ${bg_colors[key]}" role="progressbar" data-width="${e.percentage}%" aria-valuenow="${e.percentage}" aria-valuemin="0" aria-valuemax="100" style="width: ${e.percentage}%;"></div>
+                </div>
+            </div>`;
         });
         class_count_list = classes.total_classes_count == 0 ? no_content_wrapper("No Class Found", "No class has been created yet.", "fa-graduation-cap") : class_count_list;
         $(`div[id="class_count_list"]`).html(class_count_list);
--- system/config/settings.php
+++ system/config/settings.php
@@ -16,7 +16,7 @@ define('APPPATH', $application_folder.DIRECTORY_SEPARATOR);
 define('VIEWPATH', $application_folder.DIRECTORY_SEPARATOR);
 
 function version() {
-    return "1.3.2";
+    return "1.3.3";
 }
 
 function loggedIn() {
