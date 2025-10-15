e643667ca48953229e575db5a01fa305aa3321dd
Emmanuel Obeng <emmallob14@gmail.com>
2025-10-15 21:59:17 +0000

    Increment the settings version

--- system/config/settings.php
+++ system/config/settings.php
@@ -16,7 +16,7 @@ define('APPPATH', $application_folder.DIRECTORY_SEPARATOR);
 define('VIEWPATH', $application_folder.DIRECTORY_SEPARATOR);
 
 function version() {
-    return "1.4.5";
+    return "1.4.6";
 }
 
 function loggedIn() {
