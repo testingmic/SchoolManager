97810abab163dcd065b25b046063ce4775550080
Emmanuel Obeng <emmallob14@gmail.com>
2025-11-24 08:57:22 +0000

    Enhance user data processing in Users controller by adding finalize status to attendance log retrieval

--- application/controllers/users.php
+++ application/controllers/users.php
@@ -814,12 +814,13 @@ class Users extends Myschoolgh {
 				
 				/** Get the attendance log for the day */
 				$check = $this->pushQuery(
-					"a.users_list, a.users_data, a.user_type, a.class_id", "users_attendance_log a", "a.log_date='{$selected_date}' {$query} LIMIT 1"
+					"a.users_list, a.users_data, a.user_type, a.class_id, a.finalize", "users_attendance_log a", "a.log_date='{$selected_date}' {$query} LIMIT 1"
 				);
 
 				$users_list = !empty($check) ? json_decode($check[0]->users_data, true) : [];
 
 				foreach($data as $key => $user) {
+					$data[$key]->can_update = !($check[0]->finalize == 1);
 					$data[$key]->status = $users_list[$user->user_id]['state'] ?? '';
 					$data[$key]->comments = $users_list[$user->user_id]['comments'] ?? '';
 				}
